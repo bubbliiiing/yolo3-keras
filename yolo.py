2042d850cc495fc791948caf1f0f18f74987c238
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-06-11 11:50:22 +0800

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -9,6 +9,10 @@ from keras.layers import Input
 from PIL import Image, ImageFont, ImageDraw
 from nets.yolo3 import yolo_body,yolo_eval
 from utils.utils import letterbox_image
+#--------------------------------------------#
+#   使用自己训练好的模型预测需要修改2个参数
+#   model_path和classes_path都需要修改！
+#--------------------------------------------#
 class YOLO(object):
     _defaults = {
         "model_path": 'model_data/yolo_weights.h5',
