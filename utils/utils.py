ae2bba24d34988854da0b54d24ea3bf7bd20c1b7
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-07-26 21:22:55 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,7 @@ from functools import reduce
 from PIL import Image
 import numpy as np
 from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
-
+import cv2
 def compose(*funcs):
     if funcs:
         return reduce(lambda f, g: lambda *a, **kw: g(f(*a, **kw)), funcs)
@@ -61,15 +61,16 @@ def get_random_data(annotation_line, input_shape, random=True, max_boxes=20, jit
     hue = rand(-hue, hue)
     sat = rand(1, sat) if rand()<.5 else 1/rand(1, sat)
     val = rand(1, val) if rand()<.5 else 1/rand(1, val)
-    x = rgb_to_hsv(np.array(image)/255.)
-    x[..., 0] += hue
+    x = cv2.cvtColor(np.array(image,np.float32)/255, cv2.COLOR_RGB2HSV)
+    x[..., 0] += hue*360
     x[..., 0][x[..., 0]>1] -= 1
     x[..., 0][x[..., 0]<0] += 1
     x[..., 1] *= sat
     x[..., 2] *= val
-    x[x>1] = 1
+    x[x[:,:, 0]>360, 0] = 360
+    x[:, :, 1:][x[:, :, 1:]>1] = 1
     x[x<0] = 0
-    image_data = hsv_to_rgb(x) # numpy array, 0 to 1
+    image_data = cv2.cvtColor(x, cv2.COLOR_HSV2RGB) # numpy array, 0 to 1
 
     # correct boxes
     box_data = np.zeros((max_boxes,5))
