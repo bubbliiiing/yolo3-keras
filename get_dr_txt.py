835afed4bb4d3afc2e970ca82914df9f17dcdfcf
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-11-25 18:56:12 +0800

    Update get_dr_txt.py

--- get_dr_txt.py
+++ get_dr_txt.py
@@ -68,7 +68,8 @@ class mAP_YOLO(YOLO):
     def detect_image(self, image_id, image):
         f = open("./input/detection-results/"+image_id+".txt","w") 
         # 调整图片使其符合输入要求
-        boxed_image = letterbox_image(image, self.model_image_size)
+        new_image_size = (self.model_image_size[1],self.model_image_size[0])
+        boxed_image = letterbox_image(image, new_image_size)
         image_data = np.array(boxed_image, dtype='float32')
         image_data /= 255.
         image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
