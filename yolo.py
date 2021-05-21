0f5d96c23ddd04c30e46522232eecbf8a3894dda
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-05-21 19:39:55 +0800

    Add files via upload

--- yolo.py
+++ yolo.py
@@ -1,5 +1,6 @@
 import colorsys
 import os
+import time
 
 import numpy as np
 from keras import backend as K
@@ -206,6 +207,35 @@ class YOLO(object):
             del draw
 
         return image
+
+    def get_FPS(self, image, test_interval):
+        if self.letterbox_image:
+            boxed_image = letterbox_image(image, (self.model_image_size[1],self.model_image_size[0]))
+        else:
+            boxed_image = image.convert('RGB')
+            boxed_image = boxed_image.resize((self.model_image_size[1],self.model_image_size[0]), Image.BICUBIC)
+        image_data = np.array(boxed_image, dtype='float32')
+        image_data /= 255.
+        image_data = np.expand_dims(image_data, 0)
+
+        out_boxes, out_scores, out_classes = self.sess.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
+                K.learning_phase(): 0})
+
+        t1 = time.time()
+        for _ in range(test_interval):
+            out_boxes, out_scores, out_classes = self.sess.run(
+                [self.boxes, self.scores, self.classes],
+                feed_dict={
+                    self.yolo_model.input: image_data,
+                    self.input_image_shape: [image.size[1], image.size[0]],
+                    K.learning_phase(): 0})
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
 
     def close_session(self):
         self.sess.close()
