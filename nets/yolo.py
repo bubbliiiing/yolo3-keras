653eee5886cab9674f3a423b3f0c03738e5e4eb8
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-09-24 08:29:16 +0800

    Update yolo.py

--- nets/yolo.py
+++ nets/yolo.py
@@ -77,7 +77,7 @@ def get_train_model(model_body, input_shape, num_classes, anchors, anchors_mask)
         yolo_loss, 
         output_shape    = (1, ), 
         name            = 'yolo_loss', 
-        arguments       = {'input_shape' : input_shape, 'anchors' : anchors, 'anchors_mask' : anchors_mask, 'num_classes' : num_classes, 'ignore_thresh': 0.7}
+        arguments       = {'input_shape' : input_shape, 'anchors' : anchors, 'anchors_mask' : anchors_mask, 'num_classes' : num_classes, 'ignore_thresh': 0.5}
     )([*model_body.output, *y_true])
     model       = Model([model_body.input, *y_true], model_loss)
     return model
