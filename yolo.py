8bc7a96b4e0ac83c1dc786d534ef67b89aa3295a
Bubbliiiing <3323290568@qq.com>
2022-05-02 13:03:09 +0800

    update comment

--- yolo.py
+++ yolo.py
@@ -65,7 +65,6 @@ class YOLO(object):
     #---------------------------------------------------#
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
-        show_config(**self._defaults)
         for name, value in kwargs.items():
             setattr(self, name, value)
             
@@ -85,7 +84,9 @@ class YOLO(object):
         self.input_image_shape = K.placeholder(shape=(2, ))
         self.sess = K.get_session()
         self.boxes, self.scores, self.classes = self.generate()
-
+        
+        show_config(**self._defaults)
+        
     #---------------------------------------------------#
     #   载入模型
     #---------------------------------------------------#
